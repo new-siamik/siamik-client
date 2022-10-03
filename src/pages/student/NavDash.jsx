4785d4cd96812e5f64ea98102dcd1625c1225b48
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2022-10-03 22:24:02 +0700

    fix: navdash mobile

--- src/pages/student/NavDash.jsx
+++ src/pages/student/NavDash.jsx
@@ -3,12 +3,14 @@ import Collapse from "@kunukn/react-collapse";
 /* import useSWR from 'swr'
 import Swal from "sweetalert2"
 import axios from "../../api/axios";
- */import { NavLink } from "react-router-dom"
+ */import { NavLink, useLocation } from "react-router-dom"
 import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 // import { Dots } from 'loading-animations-react';
 
 export default function NavDash() {
 
+    const location = useLocation();
+    
     const options = [
         {
             id: 1,
@@ -70,15 +72,30 @@ export default function NavDash() {
     ]
 
     const [isOpen, setIsOpen] = React.useState(false);
-    const [active, setActive] = React.useState(options[0].name);
+    const [activeText, setActiveText] = React.useState();
 
     const onInit = ({ state, style, node }) => {
         setIsOpen(true);
       };
-
-    const handleClick = (e) => {
-        setActive(e.target.innerText);
+    
+    const getTargetNameWithCurrentUrl = () => {
+        options.map(option => (
+            location.pathname === option.link ?
+                setActiveText(option.name)
+            :
+            option.child?.map(child => (
+                location.pathname === child.link ?
+                    setActiveText(child.name)
+                :
+                    false
+            ))
+        ))
     }
+
+    React.useEffect(() => {
+        getTargetNameWithCurrentUrl()
+        // eslint-disable-next-line
+    }, [])
 
 /*     const { data: user } = useSWR( `http://localhost:8080/api/user/id?id=${localStorage.id}`, async url => await axios.get(url, { withCredentials: true }).then( res => res.data.users).catch( er => {
         Swal.fire({
@@ -109,31 +126,63 @@ export default function NavDash() {
                 <nav className="nav-dashboard">
                     <details className="md:hidden block inter relative w-full mr-[1rem]">
                         <summary className="summary-navside">
-                            {active}
+                            { activeText }
                         </summary>
                         <ul>
                             {
-                                options.map(option => {
-                                    return (
+                                options.map(option => (
+                                    option.hasOwnProperty('child') ?
+                                        <React.Fragment key={2}>
+                                            <li  className="flex items-center justify-between cursor-pointer have-child" onClick={() => {
+                                                setIsOpen(state => !state)
+                                            }}>
+                                                <h3>
+                                                    { option.name }
+                                                </h3>
+                                                {
+                                                    isOpen ?
+                                                        <ChevronRightIcon className="w-5 h-5 text-neutral-900" aria-hidden="true" />
+                                                    :
+                                                        <ChevronDownIcon className="w-5 h-5 text-neutral-900" aria-hidden="true" />
+                                                }
+                                            </li>
+                                            <Collapse isOpen={!isOpen} onInit={onInit} style={{margin: 0}} elementType="div" className={`collapse-css-transition`}>
+                                                    {
+                                                        option.child.map( itemChild => (
+                                                            <li key={itemChild.id} className="pt-6 pl-4 inter">
+                                                                <NavLink to={itemChild.link} 
+                                                                        className={({ isActive }) =>
+                                                                            isActive ? 'active-visit child-item' : ''
+                                                                        }
+                                                                >
+                                                                    <h3>
+                                                                        {itemChild.name}
+                                                                    </h3>
+                                                                </NavLink>
+                                                            </li>
+                                                        ))
+                                                    }
+                                            </Collapse>
+                                        </React.Fragment>
+                                    :   
                                         <li key={option.id}>
-                                            <NavLink to={option.link} onClick={handleClick}
+                                            <NavLink to={option.link} 
                                                     className={({ isActive }) =>
                                                         isActive ? 'active-visit' : ''
                                                     }
                                             >{option.name}</NavLink>
                                         </li>
-                                    )
-                                })
+                                ))
                             }
                             <li>
-                                <NavLink to="/change-password" onClick={handleClick}
+                                <NavLink to="/change-password" 
                                 className={({ isActive }) =>
                                     isActive ? 'active-visit' : ''
                                 }
                                 >Ganti Kata Sandi</NavLink>
                             </li>
                             <li>
-                                <NavLink to="/change-password" onClick={handleClick}
+                                <NavLink to="/change-password" 
                                 className={({ isActive }) =>
                                     isActive ? 'active-visit' : ''
                                 }
@@ -145,38 +194,38 @@ export default function NavDash() {
                         {
                             options.map(option => (
                                 option.hasOwnProperty('child') ?
-                                <React.Fragment key={2}>
-                                    <li className="nav-item-dashboard inter flex items-center justify-between cursor-pointer" onClick={() => {
-                                        setIsOpen(state => !state)
-                                    }}>
-                                        <h3>
-                                            { option.name }
-                                        </h3>
-                                        {
-                                            isOpen ?
-                                                <ChevronRightIcon className="w-5 h-5 text-neutral-100" aria-hidden="true" />
-                                            :
-                                                <ChevronDownIcon className="w-5 h-5 text-neutral-100" aria-hidden="true" />
-                                        }
-                                    </li>
-                                    <Collapse isOpen={!isOpen} onInit={onInit} style={{margin: 0}} elementType="div" className={`collapse-css-transition`}>
+                                    <React.Fragment key={2}>
+                                        <li className="nav-item-dashboard inter flex items-center justify-between cursor-pointer" onClick={() => {
+                                            setIsOpen(state => !state)
+                                        }}>
+                                            <h3>
+                                                { option.name }
+                                            </h3>
                                             {
-                                                option.child.map( itemChild => (
-                                                    <li key={itemChild.id} className="nav-item-dashboard pt-6 pl-4 inter">
-                                                        <NavLink to={itemChild.link} onClick={handleClick}
-                                                                className={({ isActive }) =>
-                                                                    isActive ? 'active-visit' : ''
-                                                                }
-                                                        >
-                                                            <h3>
-                                                                {itemChild.name}
-                                                            </h3>
-                                                        </NavLink>
-                                                    </li>
-                                                ))
+                                                isOpen ?
+                                                    <ChevronRightIcon className="w-5 h-5 text-neutral-100" aria-hidden="true" />
+                                                :
+                                                    <ChevronDownIcon className="w-5 h-5 text-neutral-100" aria-hidden="true" />
                                             }
-                                    </Collapse>
-                                </React.Fragment>
+                                        </li>
+                                        <Collapse isOpen={!isOpen} onInit={onInit} style={{margin: 0}} elementType="div" className={`collapse-css-transition`}>
+                                                {
+                                                    option.child.map( itemChild => (
+                                                        <li key={itemChild.id} className="nav-item-dashboard pt-6 pl-4 inter">
+                                                            <NavLink to={itemChild.link} 
+                                                                    className={({ isActive }) =>
+                                                                        isActive ? 'active-visit' : ''
+                                                                    }
+                                                            >
+                                                                <h3>
+                                                                    {itemChild.name}
+                                                                </h3>
+                                                            </NavLink>
+                                                        </li>
+                                                    ))
+                                                }
+                                        </Collapse>
+                                    </React.Fragment>
                                 :
                                     <li key={option.id} className="nav-item-dashboard inter">
                                         <NavLink to={option.link} className={({isActive}) =>
